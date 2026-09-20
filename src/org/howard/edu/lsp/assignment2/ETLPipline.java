a22e7f6c929541c3369791e6709f5aa76de51d81
Obinna84 <68216674+Obinna84@users.noreply.github.com>
2026-09-20 19:15:59 -0400

    correcting data file paths

--- src/org/howard/edu/lsp/assignment2/ETLPipline.java
+++ src/org/howard/edu/lsp/assignment2/ETLPipline.java
@@ -10,8 +10,8 @@ import org.howard.edu.lsp.assignment3.Employee;
 
 public class ETLPipline {
     public static void main(String[] args) {
-        String inputFilePath = "LSP_REPO_OBIORA9/data/employees.csv";
-        String outputFilePath = "LSP_REPO_OBIORA9/data/transformed_employees.csv";
+        String inputFilePath = "data/employees.csv";
+        String outputFilePath = "data/transformed_employees.csv";
 
         int rowsRead = 0;
         int rowsTransformed = 0;
